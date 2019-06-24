6f10c8cf0bfc2e4ea93cfaa33db6e0177fa81e41
nightblood <165374900@163.com>
2019-06-24 22:35:28 +0800

    init

--- app/src/main/java/com/zlf/bbzs/MainApplication.java
+++ app/src/main/java/com/zlf/bbzs/MainApplication.java
@@ -1,10 +1,12 @@
 package com.zlf.bbzs;
 
 import android.app.Application;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.multidex.MultiDex;
 
-import com.raizlabs.android.dbflow.config.FlowConfig;
-import com.raizlabs.android.dbflow.config.FlowManager;
-import com.zlf.bbzs.util.BarUtils;
+import com.zlf.bbzs.bean.DaoMaster;
+import com.zlf.bbzs.bean.DaoSession;
 import com.zlf.bbzs.util.Utils;
 
 /**
@@ -17,7 +19,30 @@ public class MainApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Utils.init(this);
-        FlowManager.init(new FlowConfig.Builder(this).build());
+        initGreenDao();
 
     }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
+    /**
+     * 初始化GreenDao,直接在Application中进行初始化操作
+     */
+    private void initGreenDao() {
+
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "aserbao.db");
+        SQLiteDatabase db = helper.getWritableDatabase();
+        DaoMaster daoMaster = new DaoMaster(db);
+        daoSession = daoMaster.newSession();
+    }
+
+    private DaoSession daoSession;
+    public DaoSession getDaoSession() {
+        return daoSession;
+    }
+
 }
--- app/src/main/java/com/zlf/bbzs/fragment/BreastMilkFragment.java
+++ app/src/main/java/com/zlf/bbzs/fragment/BreastMilkFragment.java
@@ -1,9 +1,31 @@
 package com.zlf.bbzs.fragment;
 
 import android.support.v4.app.Fragment;
+import android.text.format.DateUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.zlf.bbzs.MainApplication;
 import com.zlf.bbzs.R;
+import com.zlf.bbzs.bean.BreastMilkBean;
+import com.zlf.bbzs.bean.BreastMilkBeanDao;
+import com.zlf.bbzs.util.TimeUtils;
+import com.zlf.bbzs.util.Utils;
+import com.zlf.bbzs.widget.ClockViewByPath;
+
+import org.greenrobot.greendao.query.QueryBuilder;
+
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by zhu on 2019/6/19.
@@ -11,9 +33,163 @@ import com.zlf.bbzs.R;
 
 public class BreastMilkFragment extends BaseFragment {
 
+    private ClockViewByPath mClockView;
+    private Button mBtnLeft;
+    private Button mBtnRight;
+    private EditText mEtMemo;
+    private int mBtnLeftStat = 0; // 0 准备，1 暂停，2 继续
+    private int mBtnRightStat = 0;
+    private TextView mTvLeft;
+    private TextView mTvRight;
+    private Disposable mSubscribeRight;
+    private Disposable mSubscribeLeft;
+    private long mCurrIntervalLeft = 0;
+    private long mInitTimeLeft = 0;
+    private long mCurrIntervalRight = 0;
+    private long mInitTimeRight = 0;
+    private Button mBtnCommit;
+    private TextView mTvDesc;
+
     @Override
     protected void initView(View view) {
+        mClockView = view.findViewById(R.id.clock);
+        mBtnLeft = view.findViewById(R.id.btn_left);
+        mBtnRight = view.findViewById(R.id.btn_right);
+        mEtMemo = view.findViewById(R.id.et_memo);
+        mTvLeft = view.findViewById(R.id.tv_left);
+        mTvRight= view.findViewById(R.id.tv_right);
+        mBtnCommit = view.findViewById(R.id.btn_commit);
+        mTvDesc = view.findViewById(R.id.tv_desc);
 
+        mBtnLeft.setOnClickListener(view12 -> {
+            if (mBtnLeftStat == 0 || mBtnLeftStat == 2) {
+                leftStart();
+            } else if (mBtnLeftStat == 1) {
+                leftPause();
+            }
+        });
+        mBtnRight.setOnClickListener(view1 -> {
+            if (mBtnRightStat == 0 || mBtnRightStat == 2) {
+                rightStart();
+            } else if (mBtnRightStat == 1) {
+                rightPause();
+            }
+        });
+        mBtnCommit.setOnClickListener(view1 -> {
+            commit();
+        });
+        List<BreastMilkBean> list = ((MainApplication) Utils.getApp()).getDaoSession().getBreastMilkBeanDao().queryBuilder().limit(1).orderDesc(BreastMilkBeanDao.Properties.DataTime).list();
+        if (!list.isEmpty()) {
+            mTvDesc.setText(TimeUtils.getFriendlyTimeSpanByNow(list.get(0).getDataTime()));
+        } else {
+            mTvDesc.setText("还没有记录过呦！！！");
+        }
+    }
+
+    private void commit() {
+        try {
+            if (mSubscribeLeft != null) {
+                mSubscribeLeft.dispose();
+                mSubscribeLeft = null;
+                mInitTimeLeft += mCurrIntervalLeft;
+            }
+            if (mSubscribeRight != null) {
+                mSubscribeRight.dispose();
+                mSubscribeRight = null;
+                mInitTimeRight += mCurrIntervalRight;
+            }
+
+            BreastMilkBean bean = new BreastMilkBean(TimeUtils.getNowMills(), mInitTimeLeft, mInitTimeRight, mEtMemo.getText().toString());
+            ((MainApplication) Utils.getApp()).getDaoSession().getBreastMilkBeanDao().insert(bean);
+
+        } catch (Exception e) {
+            Log.e("BmFragment", e.toString());
+        }
+    }
+
+    private void rightPause() {
+        mBtnRight.setText("继续");
+        mBtnLeft.setText("右侧");
+        mBtnRightStat = 2;
+        mBtnLeftStat = 0;
+        mClockView.performAnimation();
+        if (mSubscribeLeft != null) {
+            mSubscribeLeft.dispose();
+            mSubscribeLeft = null;
+            mInitTimeLeft += mCurrIntervalLeft;
+        }
+        if (mSubscribeRight != null) {
+            mSubscribeRight.dispose();
+            mSubscribeRight = null;
+            mInitTimeRight += mCurrIntervalRight;
+        }
+    }
+
+    private void rightStart() {
+        mBtnRight.setText("暂停");
+        mBtnLeft.setText("右侧");
+        mBtnRightStat = 1;
+        mBtnLeftStat = 0;
+        mClockView.cancelAnimation();
+        if (mSubscribeRight == null) {
+            mSubscribeRight = Observable.interval(1, TimeUnit.SECONDS)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(aLong -> {
+                        mCurrIntervalRight = aLong;
+                        mTvRight.setText(timeFormat(mInitTimeRight, mCurrIntervalRight));
+                    });
+        }
+        if (mSubscribeLeft != null) {
+            mSubscribeLeft.dispose();
+            mInitTimeLeft += mCurrIntervalLeft;
+            mSubscribeLeft = null;
+        }
+    }
+
+    private void leftPause() {
+        mBtnLeft.setText("继续");
+        mBtnRight.setText("右侧");
+        mBtnLeftStat = 2;
+        mBtnRightStat = 0;
+        mClockView.start();
+        if (mSubscribeLeft != null) {
+            mSubscribeLeft.dispose();
+            mSubscribeLeft = null;
+            mInitTimeLeft += mCurrIntervalLeft;
+        }
+        if (mSubscribeRight != null) {
+            mSubscribeRight.dispose();
+            mSubscribeRight = null;
+            mInitTimeRight += mCurrIntervalRight;
+        }
+    }
+
+    private void leftStart() {
+        mBtnLeft.setText("暂停");
+        mBtnRight.setText("右侧");
+        mBtnLeftStat = 1;
+        mBtnRightStat = 0;
+        mClockView.stop();
+        if (mSubscribeLeft == null) {
+            mSubscribeLeft = Observable.interval(1, TimeUnit.SECONDS)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(aLong -> {
+                        mCurrIntervalLeft = aLong;
+                        mTvLeft.setText(timeFormat(mInitTimeLeft, mCurrIntervalLeft));
+                    });
+        }
+        if (mSubscribeRight != null) {
+            mSubscribeRight.dispose();
+            mSubscribeRight = null;
+            mInitTimeRight += mCurrIntervalRight;
+        }
+    }
+
+    private String timeFormat(long initTime, long intervalSec) {
+        long totalTime = initTime + intervalSec;
+        return String.format("%02d : %02d", totalTime / 60, totalTime % 60);
     }
 
     @Override
@@ -24,4 +200,6 @@ public class BreastMilkFragment extends BaseFragment {
     public static Fragment newInstance() {
         return new BreastMilkFragment();
     }
+
+
 }
